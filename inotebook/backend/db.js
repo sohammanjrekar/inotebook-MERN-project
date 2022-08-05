eab0e2b3332f8b98ae245a0a33cbbc1f46122603
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-08-05 23:20:28 +0530

    update

--- inotebook/backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const mongoURI="mongodb://localhost:27017/local";
-
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("connect successfully")
-    })
-}
--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -1,9 +1,6 @@
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://sohammanjrekar:soham12345@cluster0.wzrzbv3.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const mongoose = require('mongoose')
+const coonectToMongo=()=>{
+  mongoose.connect("mongodb://localhost:27017/sohamdb",()=>{
+    console.log("success");
+  })
+}
